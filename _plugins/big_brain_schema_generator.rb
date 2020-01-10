ecfdf8d6943627bee504fe9004eb71312f5bdc3e
dcljr <dcljr@me.com>
2020-01-09 18:58:32 -0800

    added types to schema

--- _plugins/big_brain_schema_generator.rb
+++ _plugins/big_brain_schema_generator.rb
@@ -10,6 +10,28 @@ module Jekyll
     def make_page()
       PageWithoutAFile.new(@site, __dir__, "", "BSMLSchema.xsd").tap do |file|
         file.content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><xs:schema id=\"BSML\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" elementFormDefault=\"unqualified\" attributeFormDefault=\"unqualified\">"
+        
+        #Attribute Types
+        valid_types = []
+        @site.data["Types"].each do |type|
+          valid_types << type["type"]
+          file.content << "<xs:simpleType name=\"" + escape_type(type["type"]) + "\"><xs:union>"
+          if type.key?("values")
+            file.content << "<xs:simpleType><xs:restriction base=\"xs:string\">"
+            type["values"].each do |value|
+              file.content << "<xs:enumeration value=\"" + value + "\" />"
+            end
+            file.content << "</xs:restriction></xs:simpleType>"
+          end
+          if type.key?("patterns")
+            type["patterns"].each do |pattern|
+              file.content << "<xs:simpleType><xs:restriction base=\"xs:string\"><xs:pattern value=\"" + pattern + "\" /></xs:restriction></xs:simpleType>"
+            end
+          end
+          file.content << "<xs:simpleType><xs:restriction base=\"xs:string\"><xs:pattern value=\"([~][a-zA-Z0-9-]*)\" /></xs:restriction></xs:simpleType></xs:union></xs:simpleType>"
+        end
+        
+        #Tags
         @site.data["Tags"].each do |tag|
           tag["aliases"].each do |tag_alias|
             file.content << "<xs:element name=\"" + tag_alias + "\" type=\"" + tag["type"] + "\"/>"
@@ -20,7 +42,7 @@ module Jekyll
               if component == type_handler["type"]
                 type_handler["properties"].each do |property|
                   property["aliases"].each do |prop_alias|
-                    current_type << "<xs:attribute name=\"" + prop_alias + "\" type=\"xs:string\"><xs:annotation><xs:documentation>" + escape_xml(property["description"]) + "</xs:documentation></xs:annotation></xs:attribute>"
+                    current_type << "<xs:attribute name=\"" + prop_alias + "\" type=\"" + (valid_types.include?(property["type"]) ? escape_type(property["type"]) : "xs:string") + "\"><xs:annotation><xs:documentation>" + escape_xml(property["description"]) + "</xs:documentation></xs:annotation></xs:attribute>"
                   end
                 end
               end
@@ -33,6 +55,8 @@ module Jekyll
           current_type << "</xs:complexType>"
           file.content << current_type
         end
+        
+        #Macros
         @site.data["Macros"].each do |macro|
           macro["aliases"].each do |macro_alias|
             file.content << "<xs:element name=\"" + macro_alias + "\" type=\"" + macro["type"] + "\"/>"
@@ -52,6 +76,9 @@ module Jekyll
     
     def escape_xml(source)
       source.gsub("\"", "&quot;").gsub("\"", "&quot;").gsub("&", "&amp;").gsub("'", "&apos;").gsub("<", "&lt;").gsub(">", "&gt;")
+    end
+    def escape_type(source)
+      source.gsub(" ", "-")
     end
   end
 end
